27b1ae3787d9780c3c7f78794e2ae070902fffa2
Anthony Fox <anthonysfox1@gmail.com>
2020-01-20 18:31:29 -0500

    Finished map project

---
+++ src/CustomMap.ts
@@ -0,0 +1,49 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+// Instructions to every other class on how they can be an argument
+// to addMarker
+// Want to create interfaces at top of the file
+// Export them so that other classes can implement an interface and it will throw errors
+// if it is missing anything
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  // Made it so no one can touch this variable outside of the class
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
